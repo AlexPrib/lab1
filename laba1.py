00bdd30f5bf5fa955e1af755e7bc1b8482a47c14
AlexPrib <alexprib03@gmail.com>
2022-09-26 20:18:01 +0400

    added years change and finder max month

--- laba1.py
+++ laba1.py
@@ -21,3 +21,25 @@ while (f == 0):
         linktmp = linktmp.replace(str(MaxYear - 1), str(MaxYear)) 
 
 linktmp = linktmp.replace(str(MaxYear+1),str(MaxYear))
+
+#замена года в ссылке
+def years_change (year, url):
+    url = url.replace(str(year - 1), str(year))
+    return url
+
+#нахождение максимального месяца
+def month_chek (url):
+    month = 1
+    f = 0
+    while (f == 0):
+      html_text = requests.get(url,headers={'User-Agent':'Windows 10'}).text
+      parse = BeautifulSoup(html_text, 'lxml')
+      errorelement = parse.find('div', class_='grey digit')
+      
+      if errorelement:
+        f = 1
+        month -= 1
+      else:
+        month+= 1
+        url = url[0:39] + '/' + str(month) + '/'
+    return month
